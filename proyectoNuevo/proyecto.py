c13e883c98ef6b2e48b134b7189d2ab9ab860214
Federico Sosa <federico.sosa4848@gmail.com>
2024-05-21 20:39:43 -0300

    creamos una clase en la rama master

--- proyectoNuevo/proyecto.py
+++ proyectoNuevo/proyecto.py
@@ -1,3 +1,10 @@
 print("hola")
 
 #profe una consulta. al final de la clase podría explicar el git config core.autocrlf , pq no se si es necesario o no
+
+class Persona():
+    def __init__(self,nombre) -> None:
+        self.nombre = nombre
+    def __str__(self) -> str:
+        return self.nombre
+    
